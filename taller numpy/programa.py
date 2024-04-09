cd7e90f87ede5923ddd75639670cc6ef970a9eec
johan bustos <jsbustose@udistrital.edu.co>
2024-04-09 15:21:38 -0500

    v0.3

--- taller numpy/programa.py
+++ taller numpy/programa.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # una forma de crear arreglos ndarray es usando una lista 
 
-miLista=[3,5,7,9]
+miLista=[3.79,5.89,7,9,"hola"]
 miArreglo= np.array(miLista)
 #Dimensiones 
 print(miArreglo.ndim)
@@ -15,3 +15,27 @@ print(miArreglo.size)
 print(miArreglo.dtype)
 
 miArreglo= np.array([3,6,7,90])
+
+#crear arreglos de 2 dimensisones a partir de listas
+
+miLista=[(1,3,5,7), (3,8,2,4)]
+miArreglo= np.array(miLista, dtype=int)
+
+print(miArreglo.ndim)
+print(miLista)
+
+
+# Usando funoiones de "relleno" de arreglos
+
+miArreglo= np.zeros((3,4))
+print(miArreglo)
+
+miArreglo= np.arange(-10,10)
+print(miArreglo.ndim)
+
+miArreglo.reshape((2,10))
+
+print(miArreglo)
+
+
+
